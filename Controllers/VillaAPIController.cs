554eabb3501f6de529102bae1a5dd40fd1b5ecf1
Rishu Rana <Rishu.Rana@assetmark.com>
2024-07-05 17:28:09 +0530

    added authentication cases in controller

--- Controllers/VillaAPIController.cs
+++ Controllers/VillaAPIController.cs
@@ -11,9 +11,28 @@ namespace WebApplication1.Controllers
     public class VillaAPIController: ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        // http end point to get all the villas 
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+
+            return Ok(VillaStore.villaList);
+        }
+        
+        [HttpGet("{id:int}")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            // addding authentication
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var  villa = (VillaStore.villaList.FirstOrDefault(u =>u.Id == id));
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+            return Ok(villa); 
         }
     }
 }
